574e923a3cdb1d1e933376c6120f48adb873f976
MohammadRezaShobiri <mohammad.shobiri@gmail.com>
2024-02-11 12:26:13 +0330

    ok build

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 // Scripts for firebase and firebase messaging
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
     apiKey: "AIzaSyCWSTknQMMfaXIQIz7R7D8ID0JvxsYZroA",
@@ -11,15 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "687205196065",
     appId: "1:687205196065:web:a704958c415de0fe645e60"
     };
-
 firebase.initializeApp(firebaseConfig);
-
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage(function(payload) {
 //   console.log('Received background message ', payload);
-
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
